4cb9c140d8f1a33fabc6120202371b6f11a9505a
rkjohny <rkjohny@gmail.com>
2016-12-27 20:01:50 +0600

    added auth module, initial version

---
+++ Dbo/src/AuthUtils.cpp
@@ -0,0 +1,32 @@
+/**
+ * Author: Rezaul Karim
+ * Email: rkjohny@gmail.com
+ *
+ * Copyright (C) 20016 Rezaul Karim (rkjohny@gmail.com) all rights reserved.
+ *
+ * The information contained here-in is the property of Rezaul Karim and is not to be
+ * disclosed or used without prior written permission of Rezaul Karim. This copyright
+ * extends to all media in which this information may be preserved including
+ * magnetic storage, computer print-out or visual display.
+ */
+
+#include "AuthUtils.h"
+#include "RndGenerator.h"
+#include "LCrypto.h"
+#include "LibCryptoDef.h"
+
+namespace  Dal {
+
+std::string AuthUtils::GenerateEmailToken()
+{
+    std::string emailToken;
+
+    LCrypto::RndGenerator *randGenerator = LCrypto::GetRndGenerator();
+    randGenerator->Initialize();
+    randGenerator->GetRandomBytes(emailToken, EMAIL_TOKEN_LENGTH);
+    randGenerator->Dispose();
+
+    return emailToken;
+}
+
+}
