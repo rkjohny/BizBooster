7123611d1cc264045faa2363664fd6deefb64280
rkjohny <rkjohny@gmail.com>
2017-01-02 00:02:31 +0600

    requester added

--- Api/include/ApiHelper.h
+++ Api/include/ApiHelper.h
@@ -75,12 +75,13 @@ public:
             CheckPermission();
 
             ExecuteHelper();
-
-            m_output->Serialize();
             
             if (m_output->GetError().GetCode() == AppErrorCode::SUCCESS) {
+                
+                m_output->Serialize();
                 //TODO: what to do  if commit failes
                 bool succeeded = dao->CommitTransaction(m_requester, transaction);
+                
             } else {
                 dao->RollbackTransaction(m_requester, transaction);
                 response = m_output->GetErrorResponse();
